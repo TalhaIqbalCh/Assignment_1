54a591a6adfc38c61cdfb95fd178285aee3dad80
Talha_Iqbal <113589282+TalhaIqbalCh@users.noreply.github.com>
2022-10-10 09:24:54 -0700

    Update Q22.cpp

--- Q22.cpp
+++ Q22.cpp
@@ -10,15 +10,16 @@ int main() {
 	srand(time(0));
 	int number1, number2;
 
-	number1 = rand() % 100000;
-	number2 = rand() % 100000;
+	number1 = rand() % 100000 + 1;
+	number2 = rand() % 100000 + 1;
 
 	cout << "Welcome To The Tution\n";
-	cout << setw(10)<< number1 <<"\n+" << setw(9) << number2 << "\n----------" << endl;
-	cout << "Press any key for the answer" << endl;
+	cout << setw(10) << number1 << "\n+" << setw(9) << number2 << "\n----------" << endl;
+	cout << "Press any key for the answer\n\n" << endl;
 
 	_getch();
 
+	cout << setw(10) << number1 << "\n+" << setw(9) << number2 << "\n----------" << endl;
 	cout << setw(10) << number1 + number2;
 
 	return 0;
