579a8b4183f201ef39ad5b16fb9a6f5c17dc7b7c
Talha_Iqbal <113589282+TalhaIqbalCh@users.noreply.github.com>
2022-10-10 08:14:48 -0700

    Update Q14.cpp

--- Q14.cpp
+++ Q14.cpp
@@ -5,25 +5,28 @@
 using namespace std;
 int main() {
 	//Payment and Interest Rate
-	float rate, N, L, Interest,Monthly_Payment,Total_Amount;
-	rate = (1.0 / 100);
+	float rate, Num_of_Payments, Amount_of_Loan, Interest, Monthly_Payment, Total_Amount;
 
 	cout << "Enter amount of Loan\n";
-	cin >> L;
+	cin >> Amount_of_Loan;
 	cout << "Enter the Number of Payments\n";
-	cin >> N;
+	cin >> Num_of_Payments;
+	cout << "Enter the Monthly Interest Rate %\n";
+	cin >> rate;
 
-	Monthly_Payment = (rate * (pow(1 + rate, N)) / (pow(1 + rate, N) - 1) ) * L;
-	cout << "Loan Amount:  "<<setw(15)<<"$" << L << endl;
-	cout << "Monthly Interest Rate:"<<setw(12)<<"1%"<< endl;
-	cout << "Number of Payments:"<<setw(15) << N << endl;
-	cout << "Monthly Payments:" <<setw(11) <<"$" << setprecision(5) << Monthly_Payment << endl;
-	Total_Amount = Monthly_Payment * N;
+	rate /= 100;
 
-	cout << "Amount Paid Back:"<<setw(9) << "$" <<setprecision(7) << Total_Amount << endl;
-	Interest = Total_Amount - L;
+	Monthly_Payment = (rate * (pow(1 + rate, Num_of_Payments)) / (pow(1 + rate, Num_of_Payments) - 1)) * Amount_of_Loan;
+	cout << "Loan Amount:  " << setw(15) << "$" << Amount_of_Loan << endl;
+	cout << "Monthly Interest Rate:" << setw(11) << rate * 100 << "% " << endl;
+	cout << "Number of Payments:" << setw(15) << Num_of_Payments << endl;
+	cout << "Monthly Payments:" << setw(11) << "$" << setprecision(5) << Monthly_Payment << endl;
+	Total_Amount = Monthly_Payment * Num_of_Payments;
 
-	cout << "Interest Paid:" <<setw(13) << "$" << setprecision(6) << Interest << endl;
+	cout << "Amount Paid Back:" << setw(9) << "$" << setprecision(7) << Total_Amount << endl;
+	Interest = Total_Amount - Amount_of_Loan;
+
+	cout << "Interest Paid:" << setw(13) << "$" << setprecision(6) << Interest << endl;
 
 
 
